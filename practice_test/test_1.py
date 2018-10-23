41b35f8130e7a03d1c08f65b4ce8633cff04930f
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-23 16:51:07 +0100

    3SH-Python adding comments & questions

--- practice_test/test_1.py
+++ practice_test/test_1.py
@@ -1,4 +1,10 @@
+#1 Create two variables – one with your name and one with your age
+#2 Create a function which prints your data as one string
+#3 Create a function which prints ANY data (two arguments) as one string
+#4 Create a function which calculates and returns the number of decades you already lived (e.g. 23 = 2 decades)
+
 # Initialising the variables
+# Answer to Question 1
 name = "Sri"
 age = "30"
 
@@ -13,13 +19,16 @@ def get_age():
     return age
 
 
+# Answer to Question 4
 def calculate_decade(given_age):
     decade = int(given_age)/10
     return int(decade)
 
 
 def answer(given_name, given_age):
+    # Answer to Question 2
     print("Your name is " + given_name)
+    # Answer to Question 3
     print("your age is " + given_age + " and you have lived for " +
           str(calculate_decade(given_age)) + " decades")
 
