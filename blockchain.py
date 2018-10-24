68c579c3bd0e5c3a4a7494c843bde9926bb60f43
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-24 10:32:29 +0100

    3SH-Python adding comments - just for record

--- blockchain.py
+++ blockchain.py
@@ -21,14 +21,26 @@ def get_input_value():
     """ Returns the input value from the user (new transaction amount) as a float. """
     return float(input('Your transaction amount please: '))
 
+
+# Get the first value
 tx_amount = get_input_value()
 add_value(tx_amount)
 
+# Get the second value also have the first value
 tx_amount = get_input_value()
 add_value(last_transaction_value=get_last_blockchain_value(),
           transaction_amount=tx_amount)
 
+
+# Get the third value and append it to the other 2 values
 tx_amount = get_input_value()
 add_value(tx_amount, get_last_blockchain_value())
 
+# Print the value
 print(blockchain)
+
+for block in blockchain:
+    print("Outputting Block")
+    print(block)
+
+print("Done!")
