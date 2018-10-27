a51aec70e120b9f9a5d8726b817660790d9a9fb1
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-27 11:58:56 +0100

    3SH-Python break keyword used

--- blockchain.py
+++ blockchain.py
@@ -42,12 +42,15 @@ while True:
     print("Please choose")
     print("1. Add a new transaction value")
     print("2. Output the blockchain blocks")
+    print("q. Quit")
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'q':
+        break
     else:
         print("Input was invalid. Please pick a value from the list!")
 
