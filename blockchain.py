5acd2a6175176ae7bada18b6b12e53ce0ca3a424
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-27 11:55:50 +0100

    3SH-Python if elif else  in while loop

--- blockchain.py
+++ blockchain.py
@@ -46,7 +46,9 @@ while True:
     if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_blockchain_value())
-    else:
+    elif user_choice == '2':
         print_blockchain_elements()
+    else:
+        print("Input was invalid. Please pick a value from the list!")
 
 print("Done!")
