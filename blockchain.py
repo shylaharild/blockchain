822d11da30390990b57b94d11fb41e75d008e87f
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-27 11:52:49 +0100

    3SH-Python if else in while loop

--- blockchain.py
+++ blockchain.py
@@ -17,24 +17,36 @@ def add_value(transaction_amount, last_transaction_value=[1]):
     blockchain.append([last_transaction_value, transaction_amount])
 
 
-def get_input_value():
+def get_transaction_value():
     """ Returns the input value from the user (new transaction amount) as a float. """
     return float(input('Your transaction amount please: '))
 
+def get_user_choice():
+    user_input = input("Your choice: ")
+    return user_input
+
+def print_blockchain_elements():
+    #Output Blockchain list to the console
+    for block in blockchain:
+        print("Outputting Block")
+        print(block)
 
 # Get the first value
-tx_amount = get_input_value()
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
 # Print the value
 print(blockchain)
 
 while True:
-    tx_amount = get_input_value()
-    add_value(tx_amount, get_last_blockchain_value())
-
-    for block in blockchain:
-        print("Outputting Block")
-        print(block)
+    print("Please choose")
+    print("1. Add a new transaction value")
+    print("2. Output the blockchain blocks")
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
 
 print("Done!")
