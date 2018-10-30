cad56e4b25c3d4a13c8d7b0152f8e07e6a9ffc80
Shyla Hari <shyla.hari@polestarglobal.com>
2018-10-30 21:28:22 +0000

    3SH-Python cleaner way of quiting the loop

--- blockchain.py
+++ blockchain.py
@@ -60,7 +60,9 @@ def verify_chain():
     # Print the value
 print(blockchain)
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose")
     print("1. Add a new transaction value")
     print("2. Output the blockchain blocks")
@@ -76,7 +78,7 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print("Input was invalid. Please pick a value from the list!")
     if not verify_chain():
