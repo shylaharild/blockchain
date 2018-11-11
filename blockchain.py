62d3dbaa23ac56dde5e75795bca6a268831fb83d
Shyla Hari <shyla.hari@polestarglobal.com>
2018-11-11 19:00:22 +0000

    SH-Python hashing the block

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,10 @@
 # Initialising our blockchain list
-blockchain = []
+genesis_block = {
+        'previous_hash': '', 
+        'index': 0, 
+        'transaction': []
+    }
+blockchain = [genesis_block]
 open_transaction = []
 owner = 'Sri'
 
@@ -24,7 +29,19 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hashed_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hashed_block = hashed_block + str(value)
+
+    print(hashed_block)
+    block = {
+        'previous_hash': hashed_block, 
+        'index': len(blockchain), 
+        'transaction': open_transaction
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -84,7 +101,8 @@ waiting_for_input = True
 while waiting_for_input:
     print("Please choose")
     print("1. Add a new transaction value")
-    print("2. Output the blockchain blocks")
+    print("2. Mine a new block")
+    print("3. Output the blockchain blocks")
     print('h: Manipulate the chain')
     print("q. Quit")
     user_choice = get_user_choice()
@@ -95,6 +113,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transaction)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
@@ -103,10 +123,10 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print("Input was invalid. Please pick a value from the list!")
-    if not verify_chain():
-        print_blockchain_elements()
-        print("Invalid Blockchain!!")
-        break
+    # if not verify_chain():
+    #     print_blockchain_elements()
+    #     print("Invalid Blockchain!!")
+    #     break
 else:
     print("User left!")
 
