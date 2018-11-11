7b00f66f7c437d566bb1fc7e50dc25fdfd178933
Shyla Hari <shyla.hari@polestarglobal.com>
2018-11-11 19:44:19 +0000

    SH-Python managing the list of participants

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transaction = []
 owner = 'Sri'
+participants = {'Sri'}
 
 
 def hash_block(block):
@@ -29,6 +30,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     """
     transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
     open_transaction.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
@@ -81,6 +84,7 @@ while waiting_for_input:
     print("1. Add a new transaction value")
     print("2. Mine a new block")
     print("3. Output the blockchain blocks")
+    print("4. Output Participants")
     print('h: Manipulate the chain')
     print("q. Quit")
     user_choice = get_user_choice()
@@ -94,6 +98,8 @@ while waiting_for_input:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
