319933a9ba491aa0ca86fe73dfcb96bd83649787
Shyla Hari <shyla.hari@polestarglobal.com>
2018-12-01 18:18:51 +0000

    SH-Python Removing the folder

--- udemy_solutions/course_materials/oop/oop-07-adding-a-block-class/block.py
+++
@@ -1,9 +0,0 @@
-from time import time
-
-class Block:
-    def __init__(self, index, previous_hash, transactions, proof, time=time()):
-        self.index = index
-        self.previous_hash = previous_hash
-        self.timestamp = time
-        self.transactions = transactions
-        self.proof = proof
