35f337bac5bba5249d00316275c2b89f0f5e8841
Shyla Hari <shyla.hari@polestarglobal.com>
2018-12-02 11:08:17 +0000

    SH-Python ading assignment_6 Q 4 answer - pickle mode

--- assignments/assignment_6.py
+++ assignments/assignment_6.py
@@ -1,6 +1,7 @@
 # 1) Write a short Python script which queries the user for input (infinite loop with exit possibility) and writes the input to a file.
 # 2) Add another option to your user interface: The user should be able to output the data stored in the file in the terminal.
 # 3) Store user input in a list (instead of directly adding it to the file) and write that list to the file – both with pickle and json.
+# 4) Adjust the logic to load the file content to work with pickled/ json data.
 import json
 import pickle
 
@@ -48,5 +49,3 @@ while input_state:
 
 print("Program Completed!")
 
-
-# 4) Adjust the logic to load the file content to work with pickled/ json data.
