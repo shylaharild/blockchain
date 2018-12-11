b22743128b6f266ce4da9c763290d41756f250ea
Shyla Hari <shyla.hari@polestarglobal.com>
2018-12-11 18:22:45 +0000

    SH-Python Broadcasting new blocks - Informing the peer nodes

--- blockchain.py
+++ blockchain.py
@@ -216,10 +216,44 @@ class Blockchain:
             self.__chain.append(block)
             self.__open_transactions = []
             self.save_data()
+            for node in self.__peer_nodes:
+                url = 'http://{}/broadcast-block'.format(node)
+                converted_block = block.__dict__.copy()
+                converted_block['transactions'] = [
+                    tx.__dict__ for tx in converted_block['transactions']]
+                try:
+                    response = requests.post(url, json={'block': converted_block})
+                    if response.status_code == 400 or response.status_code == 500:
+                        print('Block declined, needs resolving')
+                except requests.exceptions.ConnectionError:
+                    continue
             return block
         except IndexError:
             print("Minig Failed")
             return None
+
+
+    def add_block(self, block):
+        transactions = [Transaction(
+            tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']]
+        proof_is_valid = Verification.valid_proof(
+            transactions[:-1], block['previous_hash'], block['proof'])
+        hashes_match = hash_block(self.chain[-1]) == block['previous_hash']
+        if not proof_is_valid or not hashes_match:
+            return False
+        converted_block = Block(
+            block['index'], block['previous_hash'], transactions, block['proof'], block['timestamp'])
+        self.__chain.append(converted_block)
+        stored_transactions = self.__open_transactions[:]
+        for itx in block['transactions']:
+            for opentx in stored_transactions:
+                if opentx.sender == itx['sender'] and opentx.recipient == itx['recipient'] and opentx.amount == itx['amount'] and opentx.signature == itx['signature']:
+                    try:
+                        self.__open_transactions.remove(opentx)
+                    except ValueError:
+                        print('Item was already removed')
+        self.save_data()
+        return True
 
     def add_peer_node(self, node):
         """
